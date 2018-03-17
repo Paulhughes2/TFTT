acd647b6384ea826569eecd4e7341c170ca674fb
Paulhughes2 <plhgh118@hotmail.com>
2018-03-17 15:11:52 +0000

    js

--- JS/JScript.js
+++ JS/JScript.js
@@ -4,19 +4,20 @@ $(document).ready(function(){
 	$('.modal-trigger').leanModal();
 	$('#push,secton').pushpin({ top:$('#push').height() });
 	$('#C2').carousel({fullWidth: true});  
-	autoplay2() 
+	autoplay2();
 
-	function autoplay2() {
+	$("a.toscroll").on('click',function(e) {
+		var url = e.target.href;
+		var hash = url.substring(url.indexOf("#")+1);
+		$('html, body').animate({
+		scrollTop: $('#'+hash).offset().top -70
+		}, 500);
+		return false;
+	});
+});
+function autoplay2() {
 	$('#C2').carousel('next');
 	setTimeout(autoplay2, 5500);  
-	} 
-})
-$("a.toscroll").on('click',function(e) {
-	var url = e.target.href;
-	var hash = url.substring(url.indexOf("#")+1);
-	$('html, body').animate({
-	scrollTop: $('#'+hash).offset().top -70
-	}, 500);
-	return false;
-});
+} 
+
 
