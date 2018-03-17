c1b2357c3f2c73ef6899b656bda4d1fbae390e06
Paulhughes2 <plhgh118@hotmail.com>
2018-03-17 17:35:39 +0000

    js fix

--- JS/JScript.js
+++ JS/JScript.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
 	$(".button-collapse").sideNav();
-	$('.modal-trigger').leanModal();
 	$('#push,secton').pushpin({ top:$('#push').height() });
 	$('#C2').carousel({fullWidth: true});  
 	autoplay2();
